02c105098696a8b162ac5216c5b23136ebff6b40
cutz-j <cutz-j@naver.com>
2019-12-02 22:57:57 +0900

    Update

--- algorithm_book/lec6_HS.cpp
+++ algorithm_book/lec6_HS.cpp
@@ -31,10 +31,56 @@ bool hasWord(int x, int y, const string& word) {
 	return false;
 }
 
-int main(void) {
-	const string word = "YES";
-	if(hasWord(2, 2, word))
-		cout << "found" << endl;
+int q6_3(void) {
+	int C, N, S;
+	cin >> C;
+	int* res = new int[C];
 
+	int f_num1, f_num2;
+	bool friends[10][10];
+
+	for (int i = 0; i < C; i++) {
+		cin >> N >> S;
+		bool taken[10];
+		for (int j = 0; j < C; j++) {
+			std::cin >> f_num1 >> f_num2;
+			friends[f_num1][f_num2] = friends[f_num2][f_num1] = true;
+		}
+		
+
+
+
+	}
+
+
+	return 0;
+}
+
+int solver(bool taken[10], bool friends[10][10], int n) {
+	
+	bool finish = true;
+
+	for (int i = 0; i < n; i++) {
+		if (!taken[n])
+			finish = false;
+	}
+
+	if (finish)
+		return 1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!taken[i] && !taken[j] && friends[i][j])
+				taken[i] = taken[j] = true;
+		}
+
+	}
+}
+
+int main(void) {
+	/*const string word = "YES";
+	if(hasWord(2, 2, word))
+		cout << "found" << endl;*/
+	q6_3();
 	return 0;
 }
